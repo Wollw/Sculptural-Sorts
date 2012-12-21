4811dca5f923255d24916d87d0386c143e908f5d
David Shere <david.e.shere@gmail.com>
2012-12-21 15:35:50 -0800

    added the actual randomizing code

--- src/main.c
+++ src/main.c
@@ -3,11 +3,12 @@
 #include <util/delay.h>
 #include "serial.h"
 #include "sort.h"
+
 #define LENGTH(a) (sizeof(a)/sizeof(int))
 
-void print_sort_state(int a[], size_t len, size_t pos) {
+void display_sort_state(int a[], size_t len, size_t pos) {
     for (size_t i = 0; i < len; i++)
-        printf("%5s  ", i == pos? "v" : "");
+        printf("%5s ", i == pos? "v" : "");
     printf("\r\n");
     for (size_t i = 0; i < len; i++)
         printf(i < len - 1? "%5d, " : "%5d\r\n", a[i]);
@@ -16,13 +17,11 @@ void print_sort_state(int a[], size_t len, size_t pos) {
 }
 
 int main(void) {
-
-    serial_init(19200);
-
     for(size_t i = 0;; i ^= 1) {
-        printf("\r\n===Randomizing===\r\n\r\n");
-        int a[] = { 5, 2, 4, 1, -2, 4, -5 };
-        bubble_sort_apply(a, LENGTH(a), print_sort_state);
+        int a[5];
+        for (size_t i = 0; i < 5; i++) {
+            a[i] = rand();
+        }
+        bubble_sort_apply(a, LENGTH(a), display_sort_state);
     }
-
 }
