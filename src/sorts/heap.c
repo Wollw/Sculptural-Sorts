6a7433eb36657c71d8ddf3fa7a1df1eee3cc7717
David Shere <david.e.shere@gmail.com>
2012-12-27 16:34:52 -0800

    Reimplemented heap sort with a non-recursive solution.

--- src/sorts/heap.c
+++ src/sorts/heap.c
@@ -1,51 +1,42 @@
 #include "sorts.h"
 
-/* Implemented based on the CLRS recursive algorithm.
- * This should really probably be reimplemented using a
- * non-recursive algorithm. */
+void sift_down(uint8_t a[], uint8_t len, uint8_t start, uint8_t end, apply_fn fn) {
+    uint8_t root = start;
+    fn(a, len, start);
 
-#define PARENT(i) (i/2-1)
-#define LEFT(i)   (2*i+1)
-#define RIGHT(i)  (2*i+2)
-
-void max_heapify(uint8_t a[], uint8_t i, uint8_t len, uint8_t heap_size, apply_fn fn) {
-    uint8_t l = LEFT(i);
-    uint8_t r = RIGHT(i);
-    uint8_t largest;
-    fn(a, len, i);
-
-    if (l < heap_size && a[l] > a[i])
-        largest = l;
-    else
-        largest = i;
-
-    if (r < heap_size && a[r] > a[largest])
-        largest = r;
-    
-    if (largest != i) {
-        uint8_t tmp = a[i];
-        a[i] = a[largest];
-        a[largest] = tmp;
-        max_heapify(a, largest, len, heap_size, fn);
+    while (root * 2 + 1 <= end) {
+        uint8_t child = root * 2 + 1;
+        uint8_t swap = root;
+        if (a[swap] < a[child])
+            swap = child;
+        if (child + 1 <= end && a[swap] < a[child+1]) 
+            swap = child + 1;
+        if (swap != root) {
+            uint8_t tmp = a[swap];
+            a[swap] = a[root];
+            a[root] = tmp;
+            fn(a, len, swap);
+            root = swap;
+        } else
+            return;
     }
-}
-
-void build_max_heap(uint8_t a[], uint8_t len, apply_fn fn) {
-    uint8_t i; 
-    for (i = len / 2 ; i > 0; i--) {
-        max_heapify(a, i, len, len, fn);
-    }
-    max_heapify(a, i, len, len, fn);
 }
 
 void heap_sort_apply(uint8_t a[], uint8_t len, apply_fn fn) {
-    build_max_heap(a, len, fn);
-    uint8_t i;
-    for (i = len - 1; i > 0; i--) {
-        fn(a, len, i);
-        uint8_t tmp = a[0];
-        a[0] = a[i];
-        a[i] = tmp;
-        max_heapify(a, 0, len, i, fn);
+
+    uint8_t start = (len - 2) / 2;
+    while (start > 0) {
+        sift_down(a, len, start--, len - 1, fn);
+    }
+    sift_down(a, len, start, len - 1, fn);
+
+    uint8_t end = len - 1;
+    while (end > 0) {
+        fn(a, len, 0);
+        uint8_t tmp = a[end];
+        a[end] = a[0];
+        a[0] = tmp;
+        fn(a, len, end);
+        sift_down(a, len, 0, --end, fn);
     }
 }
