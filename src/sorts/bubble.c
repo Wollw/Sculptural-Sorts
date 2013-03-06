8a5895619832732a9aae60f31a4c8feb80dbab3f
David Shere <david.e.shere@gmail.com>
2013-03-06 14:09:24 -0800

    Moved tmp variable from bubble sort outside the loop.

--- src/sorts/bubble.c
+++ src/sorts/bubble.c
@@ -4,22 +4,21 @@
 
 void bubble_sort_apply(uint8_t a[], uint8_t len, apply_fn fn) {
     bool swapped;
-    uint8_t i;
+    uint8_t i, tmp;
     for (;;) {
         swapped = false;
         for (i = 0; i < len-1; i++) {
             fn(a, len, i);
             if (a[i+1] < a[i]) {
-                uint8_t t = a[i+1];
-                a[i+1] = a[i];
-                a[i] = t;
+                tmp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = tmp;
                 swapped = true;
             }
         }
         fn(a, len, i);
-        if (!swapped) {
+        if (!swapped)
             break;
-        }
     }
 }
 
