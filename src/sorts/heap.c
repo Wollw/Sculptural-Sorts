b356309e9d406273614f7a916497783aaf1504b0
David Shere <david.e.shere@gmail.com>
2013-03-13 13:17:32 -0700

    tweaked fn calls in heap.c in order to avoid displaying the same state twice in a row

--- src/sorts/heap.c
+++ src/sorts/heap.c
@@ -1,8 +1,13 @@
 #include "sorts.h"
 
+volatile int8_t cursor_last = -1;
+
 void sift_down(uint8_t a[], uint8_t len, uint8_t start, uint8_t end, apply_fn fn) {
     uint8_t root = start;
-    fn(a, len, start);
+    if (start != cursor_last) {
+        fn(a, len, start);
+        cursor_last = start;
+    }
 
     while (root * 2 + 1 <= end) {
         uint8_t child = root * 2 + 1;
@@ -15,7 +20,10 @@ void sift_down(uint8_t a[], uint8_t len, uint8_t start, uint8_t end, apply_fn fn
             uint8_t tmp = a[swap];
             a[swap] = a[root];
             a[root] = tmp;
-            fn(a, len, swap);
+            if (swap != cursor_last) {
+                fn(a, len, swap);
+                cursor_last = swap;
+            }
             root = swap;
         } else
             return;
@@ -32,11 +40,17 @@ void heap_sort_apply(uint8_t a[], uint8_t len, apply_fn fn) {
 
     uint8_t end = len - 1;
     while (end > 0) {
-        fn(a, len, 0);
+        if (cursor_last != 0) {
+            fn(a, len, 0);
+            cursor_last = 0;
+        }
         uint8_t tmp = a[end];
         a[end] = a[0];
         a[0] = tmp;
-        fn(a, len, end);
+        if (cursor_last != end) {
+            fn(a, len, end);
+            cursor_last = end;
+        }
         sift_down(a, len, 0, --end, fn);
     }
 }
